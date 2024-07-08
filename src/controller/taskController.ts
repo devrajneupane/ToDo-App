29a27de2bd0b0aa7020d3f6d39293cdc80cd1115
devrajneupane <asminneupane79@gmail.com>
2024-07-08 23:00:28 +0545

    feat: add appropriate status code to response

--- src/controller/taskController.ts
+++ src/controller/taskController.ts
@@ -1,52 +1,96 @@
 import { Request, Response } from "express";
+
+import { ITask } from "../interfaces/Task";
 import * as TaskService from "../service/taskService";
 
+/**
+ * Get all tasks
+ *
+ * @param req Request Object
+ * @param res Response Object
+ */
 export function getTasks(req: Request, res: Response) {
-  const data = TaskService.getTasks();
-  res.json(data);
-}
-
-export function getTaskById(req: Request, res: Response) {
-  const { id } = req.params;
-  const data = TaskService.getTaskById(id);
-
-  res.json(data);
-}
-
-export function createTask(req: Request, res: Response) {
-  const { body } = req;
-  TaskService.createTask(body);
-
-  res.json({
-    message: "Task created",
-    ...body,
-  });
-}
-
-export function updateTask(req: Request, res: Response) {
-  const { id } = req.params;
-  const { body } = req;
-  const data = TaskService.updateTask(id, body);
-
-  if (data) {
-    res.json(data);
+  const serviceData = TaskService.getTasks();
+  if (serviceData.data) {
+    res.status(200).send(serviceData);
   } else {
-    res.json({
-      message: "Task updated",
-      ...body,
-    });
+    res.status(404).send(serviceData);
   }
 }
 
+/**
+ * Get task by id
+ *
+ * @param req Request Object
+ * @param res Response Object
+ */
+export function getTaskById(req: Request, res: Response) {
+  const { id } = req.params;
+  const serviceData = TaskService.getTaskById(id);
+
+  if (serviceData.data) {
+    res.status(200).send(serviceData);
+  } else {
+    res.status(404).send(serviceData);
+  }
+}
+
+/**
+ * Create a new task
+ *
+ * @param req Request Object
+ * @param res Response Object
+ */
+export function createTask(req: Request, res: Response) {
+  const body: ITask = req.body;
+
+  if (Object.keys(body).length === 0) {
+    res.status(404).send({
+      error: "Task is missing",
+    });
+    return;
+  }
+
+  const serviceData = TaskService.createTask(body);
+
+  if (serviceData.data) {
+    res.status(201).send(serviceData);
+  } else {
+    res.status(404).send(serviceData);
+  }
+}
+
+/**
+ * Update a task
+ *
+ * @param req Request Object
+ * @param res Response Object
+ */
+export function updateTask(req: Request, res: Response) {
+  const { id } = req.params;
+  const { body } = req;
+  const serviceData = TaskService.updateTask(id, body);
+
+  if (serviceData.data) {
+    res.status(200).send(serviceData);
+  } else {
+    res.status(404).send(serviceData);
+  }
+}
+
+/**
+ * Delete a task
+ *
+ * @param req Request Object
+ * @param res Response Object
+ */
 export function deleteTask(req: Request, res: Response) {
   const { id } = req.params;
-  const data = TaskService.deleteTask(id);
+  const serviceData = TaskService.deleteTask(id);
 
-  if (data) {
-    res.json(data);
+  if (serviceData.data) {
+    res.status(200).send(serviceData);
   } else {
-    res.json({
-      message: "Task deleted",
-    });
+    res.status(404).send(serviceData);
   }
 }
