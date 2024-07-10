fa914c215445065d094474aad2ee446c8fa83193
devrajneupane <asminneupane79@gmail.com>
2024-07-10 07:46:08 +0545

    feat: add  `usr` add `auth` route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import config from "../config";
+
 import taskRouter from "./taskRoute";
+import userRouter from "./userRoute";
+import authRouter from "./authRoute";
 
 const router = express();
 
-// Use the taskRouter
+// Use Routes
+router.use("/auth", authRouter);
 router.use("/tasks", taskRouter);
+router.use("/users", userRouter);
 
 export default router
