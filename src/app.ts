a7fcc9632fceae72e5799c5fb46d808d1d0a6cc3
devrajneupane <asminneupane79@gmail.com>
2024-07-11 09:38:43 +0545

    chore: updates

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import HttpStatusCodes from "http-status-codes"
 
 import config from "./config";
 import router from "./routes/";
+import { genericErrorHandler, notFoundError } from "./middleware/errorHandler";
+import { requestLogger } from "./middleware/logger";
 
 // Initialize express
 const app = express();
@@ -9,11 +12,16 @@ const app = express();
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Use the router
+app.use(requestLogger)
+
 app.use(router);
 
+app.use(notFoundError);
+
+app.use(genericErrorHandler);
+
 app.get("/", (req, res) => {
-  res.status(200).send({
+  res.status(HttpStatusCodes.OK).send({
     message: "App is running",
   });
 });
