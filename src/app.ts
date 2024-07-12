0e5510f32344e015eedf044826ec0c53ad8bee19
devrajneupane <asminneupane79@gmail.com>
2024-07-12 09:56:00 +0545

    feat: add rate limitter and cors

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,45 @@
+import cors from "cors";
+import helmet from "helmet";
 import express from "express";
-import HttpStatusCodes from "http-status-codes"
+import rateLimiter from "express-rate-limit";
+import HttpStatusCodes from "http-status-codes";
 
 import config from "./config";
 import router from "./routes/";
-import { genericErrorHandler, notFoundError } from "./middleware/errorHandler";
 import { requestLogger } from "./middleware/logger";
+import { ALLOWED_ORIGINS } from "./constant/constants";
+import { genericErrorHandler, notFoundError } from "./middleware/errorHandler";
 
 // Initialize express
 const app = express();
 
+const limiter = rateLimiter({
+  windowMs: 60 * 1000,
+  limit: 10,
+  message: "Too many Request",
+});
+
+app.use(helmet());
+
+app.use(limiter);
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      console.log(origin);
+      if (!origin || ALLOWED_ORIGINS.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed"));
+      }
+    },
+  }),
+);
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-app.use(requestLogger)
+app.use(requestLogger);
 
 app.use(router);
 
---
+++ src/constant/constants.ts
@@ -0,0 +1,4 @@
+export const ALLOWED_ORIGINS = [
+  "https://www.test.com",
+  "https://www.google.com",
+];
