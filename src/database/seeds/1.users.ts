9afba6e5408b5be4a17b123221152088e6db7d57
devrajneupane <asminneupane79@gmail.com>
2024-07-19 22:47:02 +0545

    feat: add seeds

--- src/database/seeds/1.users.ts
+++ src/database/seeds/1.users.ts
@@ -15,19 +15,42 @@ export function seed(knex: Knex): Promise<void> {
     .then(() => {
       return knex(TABLE_NAME).insert([
         {
-          id: "08b8bc54-0439-45f2-ad60-ae3a17d3b0e7",
+          id: "06f44fc3-3b48-4e2e-abfe-b5abab5d933f",
+          name: "dave",
+          email: "dave@email.com",
+          password:
+            "$2b$10$EQbCiDl66wfU2Pz38q9B2eB5RByx.KCwHOdqFiWvEmNgQmrzi/HQy",
+          permissions: ROLE.ADMIN,
+        },
+        {
+          id: "80b223de-a9c8-437d-b932-8f540452bb45",
+          name: "joy",
+          email: "joy@email.com",
+          password:
+            "$2b$10$22i040zW0L44JkPc7cFOOO4U1AABmAUlLbE5HS1vL47mwxYOVx12W",
+        },
+        {
+          id: "9da1e246-03a8-4272-be8e-1cf8f551c68f",
           name: "bob",
           email: "bob@email.com",
-          password: "hashed_Password",
+          password:
+            "$2b$10$mEd2oNuZy.rMp5XQ66qKXeyJSEKcUCYJnAK1hi2ETg3E9mKiFuinO",
+        },
+        {
+          id: "08b8bc54-0439-45f2-ad60-ae3a17d3b0e7",
+          name: "maya",
+          email: "maya@email.com",
+          password:
+            "$2b$10$d6w6iAOI3IeJ0CwdfSiwoO2VPBJAHP0QjvPyfr2OtfmOvaz15ufNi",
           permissions: ROLE.ADMIN,
         },
         {
           id: "08b8bc54-0439-45f2-ad60-ae3a17d3b0e8",
-          name: "joy",
-          email: "joi@email.com",
-          password: "hashed_Passw00ord",
+          name: "fox",
+          email: "fox@email.com",
+          password:
+            "$2b$10$l8xgyHBdsgOzyWZH1gyZlO8xWY8HhGf8aYISLvKbDNjcjd2oki6Wy",
         },
       ]);
     });
 }
-
--- src/database/seeds/2.tasks.ts
+++ src/database/seeds/2.tasks.ts
@@ -15,15 +15,15 @@ export function seed(knex: Knex): Promise<void> {
     .then(() => {
       return knex(TABLE_NAME).insert([
         {
-          taskId: "08b8bc54-0439-45f2-ad60-ae3a17d3b0b7",
-          userId: "08b8bc54-0439-45f2-ad60-ae3a17d3b0e7",
+          task_id: "08b8bc54-0439-45f2-ad60-ae3a17d3b0b7",
+          user_id: "9da1e246-03a8-4272-be8e-1cf8f551c68f",
           title: "Get things done",
           desc: "Do the things that need to be done",
           status: TASK_STATUS.PENDING,
         },
         {
-          taskId: "08b8bc54-0439-45f2-ad60-ae3a17d3b0d7",
-          userId: "08b8bc54-0439-45f2-ad60-ae3a17d3b0e7",
+          task_id: "08b8bc54-0439-45f2-ad60-ae3a17d3b0d7",
+          user_id: "80b223de-a9c8-437d-b932-8f540452bb45",
           title: "Make things happen",
           desc: "Make the things that need to happen happen",
         },
